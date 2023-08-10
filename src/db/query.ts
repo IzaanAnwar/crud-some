48d54ec817ade3d2c94e19d107ca2ebac396dd40
IzaanAnwar <mdizaan67@gmail.com>
2023-08-10 18:56:48 +0530

    read table

--- src/db/query.ts
+++ src/db/query.ts
@@ -35,3 +35,56 @@ export async function createSome(
         throw error;
     }
 }
+
+export async function getAll(db: DatabaseConnection, tableName: string) {
+    const query = `SELECT * FROM ${tableName}`;
+    if (db instanceof PGClient) {
+        return await db.query(query);
+    } else if (db instanceof SQLiteDatabase) {
+        return await new Promise<any[]>((resolve, reject) => {
+            db.all(query, (error, rows) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    } else {
+        return await db.query(query);
+    }
+}
+
+type Condition = { [column: string]: any };
+type WhereClause = { [table: string]: Condition };
+export async function getWhere(db: DatabaseConnection, where: WhereClause) {
+    const tableName = Object.keys(where)[0];
+    const condition = where[tableName];
+
+    const columns = Object.keys(condition).join(" AND ");
+    const values = Object.values(condition).join(" AND ");
+
+    const query = `SELECT * FROM ${tableName} WHERE ${columns} = ${values}`;
+    let result: any;
+
+    if (db instanceof PGClient) {
+        result = await db.query(query);
+    } else if (db instanceof Database) {
+        result = await new Promise<void>((resolve, reject) => {
+            console.log("heer", query);
+
+            db.run(query, (error) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    } else {
+        result = await db.query(query);
+    }
+    console.log("r->", result);
+
+    return result;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Database } from "sqlite3";
 import { connectDatabase } from "./db/connection";
+import { createSome, getAll, getWhere } from "./db/query";
 
 async function main() {
     const db = await connectDatabase("sqlite", { fileName: "memory.db" });
@@ -8,6 +9,19 @@ async function main() {
         name: "iz",
         age: 24,
     };
+    const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS user (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+  )
+`;
+    const someData = await getWhere(db, {
+        user: {
+            age: 23,
+        },
+    });
+    console.log(someData);
 }
 
 main();
